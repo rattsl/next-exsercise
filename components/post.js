725cc16f27f81ba2fc5732441b1e319299dec701
rattsl <r4ttsl@gmail.com>
2022-02-13 17:16:09 +0900

    add: implement project1

--- components/post.js
+++ components/post.js
@@ -1,9 +1,14 @@
-const Post = ({post}) => {
-  return(
-    <li>
-      <span>{post.id}</span> : <span className="cursor-pointer text-blue-500 border-b border-blue-500">{post.title}</span>
-    </li>
-  )
+import Link from "next/link";
+export default function Post({ post }) {
+  return (
+    <div>
+      <span>{post.id}</span>
+      {" : "}
+      <Link href={`/posts/${post.id}`}>
+        <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+          {post.title}
+        </span>
+      </Link>
+    </div>
+  );
 }
-
-export default Post;
--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,20 +1,20 @@
 import Layout from "../components/Layout";
-import Post from "../components/post";
-import { getAllPostData } from "../libs/posts";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-export default function Blog({posts}){
+export default function Blog({ posts }) {
   return (
-    <Layout title="blog">
-      <ul className="mt-10">
-        {posts && posts.map( post => <Post key={post.id} post={post}/> )}
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
       </ul>
     </Layout>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const posts = await getAllPostData();
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
   return {
-    props: { posts }
+    props: { posts },
   };
 }
